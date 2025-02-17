a665edf6e102c0c1b9d9e08004f0d8364ff9afac
Gamal-Ehab <gamalehabg@gmail.com>
2025-02-17 21:41:11 +0200

    second

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { MealDetailsComponent } from './meal-details/meal-details.component';
 
 export const routes: Routes = [
     {path:'', redirectTo:'home/All', pathMatch:'full'},
-    {path:'home/All', component:MealsComponent, pathMatch:'full'},
-    {path:'home/:category', component:MealsComponent, pathMatch:'full'},
-    {path:'mealDetails/:id', component:MealDetailsComponent, pathMatch:'full', title:'Meal Details'}
+    {path:'home/All', component:MealsComponent},
+    {path:'home/:category', component:MealsComponent},
+    {path:'mealDetails/:id', component:MealDetailsComponent, title:'Meal Details'}
 ];
